cec187a4677698e74a77a7bc5ebb4f0b7e5bb391
Samuel Cruz-Lara <samuel.cruz-lara@univ-lorraine.fr>
2024-09-03 16:45:48 +0200

    Create MoneyList.java

---
+++ src/tp1/MoneyList.java
@@ -0,0 +1,17 @@
+package tp1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MoneyList {
+    private List<Money> list;
+    public MoneyList() {
+        list= new ArrayList<Money>();
+    }
+    public List<Money> getList() {
+        return list;
+    }
+    public void ajouterSomme (Money m) throws DeviseException {
+    // A compléter
+    }
+}
